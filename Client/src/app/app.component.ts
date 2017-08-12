c68aa3ad8ad5fd185defe26aca1d4011e89b3019
Youssef <youssef.sharief@thoughtdesign.com.au>
2017-08-12 10:39:43 +0200

    Commit 2

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  favoriteSeason: string;
+
+  seasons = [
+    'Winter',
+    'Spring',
+    'Summer',
+    'Autumn',
+  ];
 }
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,14 +1,24 @@
+import { MaterialModule } from './shared/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MdNativeDateModule } from '@angular/material';
 import { AppComponent } from './app.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MdNativeDateModule,
+    ReactiveFormsModule,
+    MdNativeDateModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
