cf9eaac1449ccf16ec9a935cd530e82427ce244b
Youssef <youssef.sharief@thoughtdesign.com.au>
2017-08-14 05:23:52 +0200

    Add basic arcgis

--- Client/src/app/route1/route1.component.ts
+++ Client/src/app/route1/route1.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { EsriLoaderService } from 'angular-esri-loader';
 
 @Component({
-    moduleId: module.id,
     selector: 'route1',
     templateUrl: 'route1.component.html',
-    styleUrls: ['route1.component.scss']
+    styleUrls: ['route1.component.css']
 })
 export class Route1Component {
+    @ViewChild('mapViewNode') mapEl: ElementRef;
 
+    map: any;
+
+    constructor(private esriLoader: EsriLoaderService) { }
+    ngOnInit() {
+        // only load the ArcGIS API for JavaScript when this component is loaded
+        return this.esriLoader.load({
+            // use a specific version of the API instead of the latest
+            url: '//js.arcgis.com/3.18/'
+        }).then(() => {
+            // load the map class needed to create a new map
+            this.esriLoader.loadModules(['esri/map']).then(([map]) => {
+                // create the map at the DOM element in this component
+                this.map = new map(this.mapEl.nativeElement, {
+                    center: [-118, 34.5],
+                    zoom: 8,
+                    basemap: 'dark-gray'
+                });
+            });
+        });
+    }
 }
--- Client/src/app/route1/route1.module.ts
+++ Client/src/app/route1/route1.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 
 // This Module's Components
 import { Route1Component } from './route1.component';
+import { EsriLoaderModule } from 'angular-esri-loader';
+import { Route1RoutingModule } from './route1-routing.module';
 
 @NgModule({
     imports: [
-        SharedModule
+        Route1RoutingModule,
+        SharedModule,
+        EsriLoaderModule
+
+
     ],
     declarations: [
         Route1Component,
