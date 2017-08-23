4fb26f7ee5a44c4888a39c6325975430df363168
Youssef <youssef.sharief@thoughtdesign.com.au>
2017-08-23 19:47:18 +0200

    Add auth spec

--- Server/src/core/authentication.js
+++ Server/src/core/authentication.js
@@ -1,35 +1,35 @@
 
 const jwt = require('jsonwebtoken');
-
+const _ = require('lodash')
+var fs = require('fs')
 
 module.exports = {
-    setSecrets(app, config) {
+    getTokenFromRequest,
+    jwtVerification,
+    setSecrets: (app, config) => {
         app.set('adminSecret', config.adminSecret);
         app.set('mobileSecret', config.mobileSecret);
     },
-    verifyUser(req, res, next) {
-        const token = req.body.token || req.params.token || req.headers['x-access-token'];
-        if (!token) return res.status(401).send('No token provided.');
-        if (token) {
-            jwt.verify(token, req.app.get('mobileSecret'), function (err, decoded) {
-                if (err) return res.status(401).send('Failed to authenticate token.');
-                req.decoded = decoded;
-                next();
-            });
-        }
-    },
-    verifyAdmin(req, res, next) {
-        let token;
-        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer')
-        { token = req.headers.authorization.split(' ')[1] }
-        if (!token) return res.status(401).send('No token provided for admin.');
-        if (token) {
-            jwt.verify(token, req.app.get('adminSecret'), function (err, decoded) {
-                if (err) return res.status(401).send('Failed to authenticate token.');
-                req.decoded = decoded;
-                next();
-            });
-        }
-    },
-
+    verifyMobile: (req, res, next) => verifyBasedOnUser(req, res, next, 'mobileSecret'),
+    verifyAdmin: (req, res, next) => verifyBasedOnUser(req, res, next, 'adminSecret')
 };
+
+function verifyBasedOnUser(req, res, next, secret) {
+    const token = getTokenFromRequest(req)
+    if (!token) return res.status(401).send('No token provided for admin.');
+    if (token) jwtVerification(token, req.app.get(secret), req, res, next)
+}
+    
+function getTokenFromRequest(req) {
+    const hasField = _.has(req, "headers.authorization")
+    if (hasField) return req.headers.authorization.split(' ')[1]
+    else return false
+}
+
+function jwtVerification(token, secret, req, res, next) {
+    return jwt.verify(token, secret, function (err, decoded) {
+        if (err) return res.status(401).send('Failed to authenticate token.');
+        req.decoded = decoded;
+        next();
+    });
+}
--- Server/src/core/authentication.spec.js
+++ Server/src/core/authentication.spec.js
@@ -1,6 +1,6 @@
 const app = require('../../src/app')
-const auth = require('../../src/core/authentication')
 require('jasmine')
+const auth = require("./authentication.js");
 
 describe("Auth module", function () {
     it("should set mobile and admin secrets", function () {
@@ -8,5 +8,31 @@ describe("Auth module", function () {
         expect(app.get('adminSecret')).toBe('123')
         expect(app.get('mobileSecret')).toBe('321')
     });
+
+
+    describe("Get Token From Request", function () {
+        it("should get token if request object is set", function () {
+            const req = {
+                headers: {
+                    authorization: "Bearer klug89"
+                }
+            }
+            expect(auth.getTokenFromRequest(req)).toBe('klug89')
+
+        });
+
+        it("should return false if headers are not set", function () {
+            const req = {
+                headers: {}
+            }
+            expect(auth.getTokenFromRequest(req)).toBe(false)
+        });
+
+        it("should return false if there is no headers field", function () {
+            const req = {}
+            expect(auth.getTokenFromRequest(req)).toBe(false)
+        });
+
+    })
 
 })
