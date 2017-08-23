e5ace165f1afe1cf307687ed715f5f71bd44388b
Youssef <youssef.sharief@thoughtdesign.com.au>
2017-08-23 19:47:28 +0200

    Add winston logger with colors

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const path = require('path');
 const authentication = require('./core/authentication')
-const logger = require('morgan');
+const morganLogger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const jwtConfig = require("./config/jwtConfig")
 const routesMiddleware = require("./routes/routesMiddleware")
 const errorHandlersMiddleware = require("./core/errorHandlersMiddleware")
+const winstonLogger = require('./core/logger')
 
 const app = express();
-
+winstonLogger.configure()
 authentication.setSecrets(app, jwtConfig)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(logger('dev'));
+app.use(morganLogger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
