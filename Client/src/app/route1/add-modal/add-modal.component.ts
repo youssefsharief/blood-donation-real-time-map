317968bdbadc6b5df58136e95fe7f360aa670507
Youssef <youssef.sharief@thoughtdesign.com.au>
2017-09-01 17:07:30 +0200

    Add form validation

--- Client/src/app/route1/add-modal/add-modal.component.ts
+++ Client/src/app/route1/add-modal/add-modal.component.ts
@@ -1,12 +1,21 @@
 import { Component, ViewChild } from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { globalValidators } from '../../shared/global-validators';
 
 @Component({
     selector: 'add-modal',
     templateUrl: 'add-modal.component.html',
 })
 export class AddModalComponent {
+    form: FormGroup
+    bloodGroups = ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-']
     @ViewChild('modal') private modal: ModalDirective
+    constructor(private fb: FormBuilder){}
+
+    ngOnInit() {
+        this.buildForm()
+    }
 
     show() {
         this.modal.show();
@@ -15,6 +24,20 @@ export class AddModalComponent {
         this.modal.hide();
     }
 
+    buildForm() {
+        this.form = this.fb.group ({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['',  globalValidators.mailFormat],
+            telephone: ['', globalValidators.telephoneFormat],
+            bloodGroup: ['', Validators.required],
+        })
+    }
 
-    
+    isIncorrectMailFormat(control){
+        return this.form.get(control).hasError('incorrectMailFormat')
+    }
+    isIncorrectTelephoneFormat(control){
+        return this.form.get(control).hasError('incorrectTelephoneFormat')
+    }
 }
---
+++ Client/src/app/shared/global-validators.ts
@@ -0,0 +1,23 @@
+
+export const globalValidators = {
+    mailFormat(control): ValidationResult {
+       var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+       if (control.value != "" && (control.value.length <= 5 || !EMAIL_REGEXP.test(control.value))) {
+           return { "incorrectMailFormat": true };
+       }
+       return null;
+   },
+
+   telephoneFormat(control): ValidationResult {
+var PHONE_REGEXP = /(00|\+)\d{12}$/i;
+if (control.value != "" && (control.value.length <= 5 || !PHONE_REGEXP.test(control.value))) {
+        return { "incorrectTelephoneFormat": true };
+    }
+    return null;
+}
+}
+
+interface ValidationResult {
+   [key: string]: boolean;
+}
+
