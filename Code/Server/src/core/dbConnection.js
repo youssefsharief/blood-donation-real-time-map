10132d85263ff77f7b4fee59687d3b4ad24e9b9e
Youssef <youssef.sharief@thoughtdesign.com.au>
2017-09-04 05:51:31 +0200

    Db uri

--- Code/Server/src/core/dbConnection.js
+++ Code/Server/src/core/dbConnection.js
@@ -1,12 +1,12 @@
 
-const dbConfing= require('../config/dbConfig.js')
+// const dbConfing= require('../config/dbConfig.js')
 const mongoose = require("mongoose")
 
 function connectToOriginalDb(){
-    return mongoose.connect(`mongodb://${dbConfing.userName}:${dbConfing.password}@ds163711.mlab.com:63711/temp_database`, { useMongoClient: true});
+    return mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true});
 }
 function connectToTestDb(){
-    return mongoose.connect(`mongodb://${dbConfing.userName}:${dbConfing.password}@ds163711.mlab.com:63711/temp_database`, { useMongoClient: true});
+    return mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true});
 }
 mongoose.connection.on('open', () => console.log('Db connected'))
 
@@ -14,3 +14,5 @@ mongoose.connection.on('error', (error) => {
     throw new Error(error)
 })
 module.exports  = {connectToOriginalDb, connectToTestDb}
+
+
