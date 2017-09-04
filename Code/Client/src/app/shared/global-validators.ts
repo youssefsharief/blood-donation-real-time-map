8509f20fc0d9ece407c646a06901515c3e0dc991
Youssef <youssef.sharief@thoughtdesign.com.au>
2017-09-04 12:18:52 +0200

    All good

--- Code/Client/src/app/shared/global-validators.ts
+++ Code/Client/src/app/shared/global-validators.ts
@@ -2,7 +2,7 @@
 export const globalValidators = {
     mailFormat(control): ValidationResult {
        var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-       if (control.value != "" && (control.value.length <= 5 || !EMAIL_REGEXP.test(control.value))) {
+       if (control.value && control.value != "" && (control.value.length <= 5 || !EMAIL_REGEXP.test(control.value))) {
            return { "incorrectMailFormat": true };
        }
        return null;
@@ -10,7 +10,7 @@ export const globalValidators = {
 
    telephoneFormat(control): ValidationResult {
 var PHONE_REGEXP = /(00|\+)\d{12}$/i;
-if (control.value != "" && (control.value.length <= 5 || !PHONE_REGEXP.test(control.value))) {
+if (control.value && control.value != "" && (control.value.length <= 5 || !PHONE_REGEXP.test(control.value))) {
         return { "incorrectTelephoneFormat": true };
     }
     return null;
--- Code/Server/src/public/2.chunk.js
+++ Code/Server/src/public/2.chunk.js
@@ -232,14 +232,14 @@ PostingModule = __decorate([
 var globalValidators = {
     mailFormat: function (control) {
         var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-        if (control.value != "" && (control.value.length <= 5 || !EMAIL_REGEXP.test(control.value))) {
+        if (control.value && control.value != "" && (control.value.length <= 5 || !EMAIL_REGEXP.test(control.value))) {
             return { "incorrectMailFormat": true };
         }
         return null;
     },
     telephoneFormat: function (control) {
         var PHONE_REGEXP = /(00|\+)\d{12}$/i;
-        if (control.value != "" && (control.value.length <= 5 || !PHONE_REGEXP.test(control.value))) {
+        if (control.value && control.value != "" && (control.value.length <= 5 || !PHONE_REGEXP.test(control.value))) {
             return { "incorrectTelephoneFormat": true };
         }
         return null;
