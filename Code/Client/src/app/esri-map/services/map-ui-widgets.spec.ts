71e2b7afa309b2de2f132318d65f09f54670f719
Youssef <youssef.sharief@thoughtdesign.com.au>
2017-09-07 06:47:45 +0200

    Update specs

--- Code/Client/src/app/esri-map/services/map-ui-widgets.spec.ts
+++ Code/Client/src/app/esri-map/services/map-ui-widgets.spec.ts
@@ -20,13 +20,3 @@ describe('Mao UI widgets helper', ()=>{
 
 
 
-
-// export function addUIWidgets(view, Track, Search) {
-//     const track = new Track({ view: view });
-//     const searchWidget = new Search({ view: view });
-//     view.ui.add(track, "top-left");
-//     view.ui.add(searchWidget, {
-//         position: "top-left",
-//         index: 2
-//     });
-// }
